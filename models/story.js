e1ffe2c1a61f515df3bfd1ca785f5f2d37e0e584
timoxa <timoxa17@gmail.com>
2018-07-29 13:10:55 +0300

    show all stories to admin and own stories for non-admin

--- models/story.js
+++ models/story.js
@@ -27,20 +27,27 @@ module.exports = class Story extends Resource {
 		return 'stories';
 	}
 
-	static indexQuery(req) {
-		const conditions = {};
-		if (req.query.user) conditions.user = req.query.user;
+	static async indexQuery(req) {
+		const conditions = {},
+			user = await req.loadUser();
+		// let admin filter by user
+		if (user.admin) {
+			if (req.query.user) conditions.user = req.query.user;
+		}
+		// non-admin users can see only their own stories
+		else conditions.user = user._id;
 		return this.find(conditions, { populate: ['user']});
 	}
 
 	static async allowCreate(req) {
 		// check user quota
 		const user = await req.loadUser();
-		if (user.quota === null || user.quota === undefined) return true;
+		console.log(user);
 		if (!user) return false;
 		if (user.quota === 0)
 			// eslint-disable-next-line
 			throw { statusCode: 403, jsonData: { message: 'Permission denied', quota: 0 } };
+		if (!user.quota) return true;
 		const count = await this.count({ user: user._id });
 		if (user.quota > count) return true;
 		// eslint-disable-next-line
@@ -58,7 +65,10 @@ module.exports = class Story extends Resource {
 	}
 
 	async fillFromRequest(req) {
-		const user = await req.loadUser();
-		this.user = user._id;
+		// set current user as an author of this story
+		if (!this.user) {
+			const user = await req.loadUser();
+			this.user = user._id;
+		}
 	}
 };
